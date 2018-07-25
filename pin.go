bb8a9a7e12f8a13613b4171ab0d2d60cc3d3df60
Cedric Fung <cedric@vec.io>
2018-07-25 20:18:32 +0800

    encrypt PIN method

---
+++ pin.go
@@ -0,0 +1,57 @@
+package bot
+
+import (
+	"bytes"
+	"context"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/pem"
+	"errors"
+	"io"
+	"time"
+)
+
+func EncryptPIN(ctx context.Context, pin, pinToken, sessionId, privateKey string, iterator uint64) (string, error) {
+	privBlock, _ := pem.Decode([]byte(privateKey))
+	if privBlock == nil {
+		return "", errors.New("invalid pem private key")
+	}
+	priv, err := x509.ParsePKCS1PrivateKey(privBlock.Bytes)
+	if err != nil {
+		return "", err
+	}
+	token, _ := base64.StdEncoding.DecodeString(pinToken)
+	keyBytes, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, priv, token, []byte(sessionId))
+	if err != nil {
+		return "", err
+	}
+	pinByte := []byte(pin)
+	timeBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(timeBytes, uint64(time.Now().Unix()))
+	pinByte = append(pinByte, timeBytes...)
+	iteratorBytes := make([]byte, 8)
+	binary.LittleEndian.PutUint64(iteratorBytes, iterator)
+	pinByte = append(pinByte, iteratorBytes...)
+	padding := aes.BlockSize - len(pinByte)%aes.BlockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	pinByte = append(pinByte, padtext...)
+	block, err := aes.NewCipher(keyBytes)
+	if err != nil {
+		return "", err
+	}
+	ciphertext := make([]byte, aes.BlockSize+len(pinByte))
+	iv := ciphertext[:aes.BlockSize]
+	_, err = io.ReadFull(rand.Reader, iv)
+	if err != nil {
+		return "", err
+	}
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], pinByte)
+	return base64.StdEncoding.EncodeToString(ciphertext), nil
+}
