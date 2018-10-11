9ad3e089d3d150b786facad198125f5953b7a2af
Crossle Song <crosslesong@gmail.com>
2018-10-11 16:58:46 +0800

    Create User API

--- user.go
+++ user.go
@@ -7,10 +7,35 @@ import (
 
 type User struct {
 	UserId         string `json:"user_id"`
+	SessionId      string `json:"session_id"`
+	PinToken       string `json:"pin_token"`
 	IdentityNumber string `json:"identity_number"`
 	FullName       string `json:"full_name"`
 	AvatarURL      string `json:"avatar_url"`
 	CreatedAt      string `json:"created_at"`
+}
+
+func CreateUser(ctx context.Context, sessionSecret, fullName, acccessToken string) (*User, error) {
+	data, err := json.Marshal(map[string]string{
+		"session_secret": sessionSecret,
+		"full_name":      fullName,
+	})
+	body, err := Request(ctx, "POST", "/users", data, acccessToken)
+	if err != nil {
+		return nil, ServerError(ctx, err)
+	}
+	var resp struct {
+		Data  *User `json:"data"`
+		Error Error `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, BadDataError(ctx)
+	}
+	if resp.Error.Code > 0 {
+		return nil, resp.Error
+	}
+	return resp.Data, nil
 }
 
 func UserMe(ctx context.Context, accessToken string) (*User, error) {
