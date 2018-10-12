f5256c65454293dd1cc117b8ef81c3258787980e
Crossle Song <crosslesong@gmail.com>
2018-10-13 03:02:43 +0800

    Update PIN api

--- user.go
+++ user.go
@@ -38,6 +38,25 @@ func CreateUser(ctx context.Context, sessionSecret, fullName, acccessToken strin
 	return resp.Data, nil
 }
 
+func UpdatePin(ctx context.Context, oldEncryptedPin, encryptedPin, accessToken string) error {
+	data, _ := json.Marshal(map[string]string{"old_pin": oldEncryptedPin, "pin": encryptedPin})
+	body, err := Request(ctx, "POST", "/pin/update", data, accessToken)
+	if err != nil {
+		return ServerError(ctx, err)
+	}
+	var resp struct {
+		Error Error `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return ServerError(ctx, err)
+	}
+	if resp.Error.Code > 0 {
+		return resp.Error
+	}
+	return nil
+}
+
 func UserMe(ctx context.Context, accessToken string) (*User, error) {
 	body, err := Request(ctx, "GET", "/me", nil, accessToken)
 	if err != nil {
