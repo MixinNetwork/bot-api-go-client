be196cd35d3a6d4fd9f24752b1e8d28726204e24
Crossle Song <crosslesong@gmail.com>
2018-10-18 15:51:50 +0800

    Create conversation API

--- conversation.go
+++ conversation.go
@@ -24,6 +24,43 @@ type Conversation struct {
 	Participants []Participant `json:"participants"`
 }
 
+func CreateConversation(ctx context.Context, category, conversationId string, participants []Participant, uid, sid, key string) (*Conversation, error) {
+	params, err := json.Marshal(map[string]interface{}{
+		"category":        category,
+		"conversation_id": conversationId,
+		"participants":    participants,
+	})
+	if err != nil {
+		return nil, err
+	}
+	accessToken, err := SignAuthenticationToken(uid, sid, key, "POST", "/conversations", string(params))
+	if err != nil {
+		return nil, err
+	}
+	body, err := Request(ctx, "POST", "/conversations", params, accessToken)
+	if err != nil {
+		return nil, err
+	}
+	var resp struct {
+		Data  Conversation `json:"data"`
+		Error Error        `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.Error.Code > 0 {
+		if resp.Error.Code == 401 {
+			return nil, AuthorizationError(ctx)
+		} else if resp.Error.Code == 403 {
+			return nil, ForbiddenError(ctx)
+		}
+		return nil, ServerError(ctx, resp.Error)
+	}
+	return &resp.Data, nil
+}
+
 func ConversationShow(ctx context.Context, conversationId string, accessToken string) (*Conversation, error) {
 	body, err := Request(ctx, "GET", "/conversations/"+conversationId, nil, accessToken)
 	if err != nil {
