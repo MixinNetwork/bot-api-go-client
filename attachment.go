8d7c7509eff935bcaff2945285059a0d2554dc57
Li Yuqing <im.yuqlee@gmail.com>
2019-04-12 17:37:00 +0800

    More api for Golang SDK

---
+++ attachment.go
@@ -0,0 +1,40 @@
+package bot
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type Attachment struct {
+	Type         string `json:"type"`
+	AttachmentId string `json:"attachment_id"`
+	ViewURL      string `json:"view_url"`
+}
+
+func AttachemntShow(ctx context.Context, uid, sid, key, id string) (*Attachment, error) {
+	token, err := SignAuthenticationToken(uid, sid, key, "GET", "/attachments/"+id, "")
+	if err != nil {
+		return nil, err
+	}
+	body, err := Request(ctx, "GET", "/attachments/"+id, nil, token)
+	if err != nil {
+		return nil, err
+	}
+	var resp struct {
+		Data  Attachment `json:"data"`
+		Error Error      `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
+	}
+	if resp.Error.Code > 0 {
+		if resp.Error.Code == 401 {
+			return nil, AuthorizationError(ctx)
+		} else if resp.Error.Code == 403 {
+			return nil, ForbiddenError(ctx)
+		}
+		return nil, resp.Error
+	}
+	return &resp.Data, nil
+}
