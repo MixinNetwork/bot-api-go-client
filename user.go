41420b38071fcb8c980a1a60afd86a0eab882c6a
Crossle Song <crosslesong@gmail.com>
2019-11-30 22:36:52 +0800

    More user API

--- user.go
+++ user.go
@@ -3,6 +3,7 @@ package bot
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 )
 
 type User struct {
@@ -27,6 +28,54 @@ func CreateUser(ctx context.Context, sessionSecret, fullName, uid, sid, sessionK
 		return nil, err
 	}
 	body, err := Request(ctx, "POST", "/users", data, token)
+	if err != nil {
+		return nil, ServerError(ctx, err)
+	}
+	var resp struct {
+		Data  *User `json:"data"`
+		Error Error `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, BadDataError(ctx)
+	}
+	if resp.Error.Code > 0 {
+		return nil, resp.Error
+	}
+	return resp.Data, nil
+}
+
+func GetUser(ctx context.Context, userId, uid, sid, sessionKey string) (*User, error) {
+	url := fmt.Sprintf("/users/%s", userId)
+	token, err := SignAuthenticationToken(uid, sid, sessionKey, "GET", url, "")
+	if err != nil {
+		return nil, err
+	}
+	body, err := Request(ctx, "GET", url, nil, token)
+	if err != nil {
+		return nil, ServerError(ctx, err)
+	}
+	var resp struct {
+		Data  *User `json:"data"`
+		Error Error `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, BadDataError(ctx)
+	}
+	if resp.Error.Code > 0 {
+		return nil, resp.Error
+	}
+	return resp.Data, nil
+}
+
+func SearchUser(ctx context.Context, mixinId, uid, sid, sessionKey string) (*User, error) {
+	url := fmt.Sprintf("/search/%s", mixinId)
+	token, err := SignAuthenticationToken(uid, sid, sessionKey, "GET", url, "")
+	if err != nil {
+		return nil, err
+	}
+	body, err := Request(ctx, "GET", url, nil, token)
 	if err != nil {
 		return nil, ServerError(ctx, err)
 	}
@@ -92,11 +141,11 @@ func UserMe(ctx context.Context, accessToken string) (*User, error) {
 
 func UpdateUserMe(ctx context.Context, uid, sid, privateKey, fullName, avatarBase64 string) (*User, error) {
 	data, err := json.Marshal(map[string]interface{}{
-		"full_name": fullName,
+		"full_name":     fullName,
 		"avatar_base64": avatarBase64,
 	})
 	if err != nil {
-		return nil , err
+		return nil, err
 	}
 
 	path := "/me"
