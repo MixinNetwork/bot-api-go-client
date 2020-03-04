a91b5b1c97f678963cfe3bf528b812b3c7679f75
Li Yuqing <im.yuqlee@gmail.com>
2020-03-04 22:52:43 +0800

    Fix snapshot fields

--- snapshot.go
+++ snapshot.go
@@ -9,21 +9,25 @@ import (
 )
 
 type Snapshot struct {
-	Type            string    `json:"type"`
-	SnapshotId      string    `json:"snapshot_id"`
-	Receiver        string    `json:"receiver"`
-	TransactionHash string    `json:"transaction_hash"`
-	OutputIndex     int64     `json:"output_index"`
-	AssetId         string    `json:"asset_id"`
-	Amount          string    `json:"amount"`
-	OpeningBalance  string    `json:"opening_balance"`
-	ClosingBalance  string    `json:"closing_balance"`
-	TraceId         string    `json:"trace_id"`
-	Memo            string    `json:"memo"`
-	Confirmations   int64     `json:"confirmations,omitempty"`
-	CreatedAt       time.Time `json:"created_at"`
-	State           string    `json:"state,omitempty"`     
-	Fee             struct {
+	Type           string    `json:"type"`
+	SnapshotId     string    `json:"snapshot_id"`
+	AssetId        string    `json:"asset_id"`
+	Amount         string    `json:"amount"`
+	OpeningBalance string    `json:"opening_balance"`
+	ClosingBalance string    `json:"closing_balance"`
+	CreatedAt      time.Time `json:"created_at"`
+	// deposit &  withdrawal
+	TransactionHash string `json:"transaction_hash,omitempty"`
+	OutputIndex     int64  `json:"output_index,omitempty"`
+	Sender          string `json:"sender,omitempty"`
+	Receiver        string `json:"receiver,omitempty"`
+	// transfer
+	OpponentId    string `json:"opponent_id,omitempty"`
+	TraceId       string `json:"trace_id,omitempty"`
+	Memo          string `json:"memo,omitempty"`
+	Confirmations int64  `json:"confirmations,omitempty"`
+	State         string `json:"state,omitempty"`
+	Fee           struct {
 		Amount  string `json:"amount"`
 		AssetId string `json:"asset_id"`
 	} `json:"fee,omitempty"`
