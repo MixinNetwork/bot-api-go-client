966672b4aef3d430f81fa052abbb88262525b3d6
Crossle Song <crosslesong@gmail.com>
2020-06-22 11:14:28 +0800

    Add turn API

---
+++ turn.go
@@ -0,0 +1,35 @@
+package bot
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type Turn struct {
+	Url        string `json:"url"`
+	Username   string `json:"username"`
+	Credential string `json:"credential"`
+}
+
+func GetTurnServer(ctx context.Context, uid, sid, sessionKey string) (*[]Turn, error) {
+	token, err := SignAuthenticationToken(uid, sid, sessionKey, "GET", "/turn", "")
+	if err != nil {
+		return nil, err
+	}
+	body, err := Request(ctx, "GET", "/turn", nil, token)
+	if err != nil {
+		return nil, ServerError(ctx, err)
+	}
+	var resp struct {
+		Data  *[]Turn `json:"data"`
+		Error Error   `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, BadDataError(ctx)
+	}
+	if resp.Error.Code > 0 {
+		return nil, resp.Error
+	}
+	return resp.Data, nil
+}
