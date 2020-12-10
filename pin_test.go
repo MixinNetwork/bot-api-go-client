f84cda01343e5e18e3b70b7420bb1ab854d3f40b
Crossle Song <crosslesong@gmail.com>
2020-12-10 23:54:05 +0800

    Use x25519 instead of ScalarMult

--- pin_test.go
+++ pin_test.go
@@ -14,16 +14,19 @@ func TestCurve25519Conversion(t *testing.T) {
 	public, private, _ := ed25519.GenerateKey(rand.Reader)
 	fmt.Println(public)
 	fmt.Println(private.Seed())
-	var curve25519Public, curve25519Public2, curve25519Private [32]byte
+	var curve25519Public2, curve25519Private [32]byte
 	PrivateKeyToCurve25519(&curve25519Private, private)
-	curve25519.ScalarBaseMult(&curve25519Public, &curve25519Private)
+	curve25519Public, err := curve25519.X25519(curve25519Private[:], curve25519.Basepoint)
+	if err != nil {
+		t.Fatalf("PublicKeyToCurve25519 failed")
+	}
 
 	if err := PublicKeyToCurve25519(&curve25519Public2, public); err != nil {
 		t.Fatalf("PublicKeyToCurve25519 failed")
 	}
 
 	fmt.Println(curve25519Public)
-	if !bytes.Equal(curve25519Public[:], curve25519Public2[:]) {
+	if !bytes.Equal(curve25519Public, curve25519Public2[:]) {
 		t.Errorf("Values didn't match: curve25519 produced %x, conversion produced %x", curve25519Public[:], curve25519Public2[:])
 	}
 }
