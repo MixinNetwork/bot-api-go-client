ecda5ea5d98ea5a21f3bc4b20a3b2aea71d3659b
Crossle Song <crosslesong@gmail.com>
2021-07-17 00:36:24 +0400

    Add external proxy API

--- network.go
+++ network.go
@@ -6,6 +6,23 @@ import (
 	"net/url"
 )
 
+func CallMixinRPC(ctx context.Context, method string, params ...interface{}) ([]byte, error) {
+	p := map[string]interface{}{
+		"method": method,
+		"params": params,
+	}
+	b, err := json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := Request(ctx, "GET", "/external/proxy", b, "")
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
 func ExternalAdddressCheck(ctx context.Context, asset, destination, tag string) (*Address, error) {
 	values := url.Values{}
 	if destination != "" {
