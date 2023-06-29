0716cc7a60bdeb58bddd2a878711bc45670d9bb4
LiYuqing <im.yuqlee@gmail.com>
2023-06-29 12:58:05 +0800

    Add UniqueObjectId

--- uuid.go
+++ uuid.go
@@ -1,6 +1,8 @@
 package bot
 
 import (
+	"crypto/md5"
+	"io"
 	"log"
 
 	"github.com/gofrs/uuid"
@@ -22,4 +24,19 @@ func UuidFromString(id string) (uuid.UUID, error) {
 
 func UuidFromBytes(input []byte) (uuid.UUID, error) {
 	return uuid.FromBytes(input)
+}
+
+func UniqueObjectId(args ...string) string {
+	h := md5.New()
+	for _, s := range args {
+		io.WriteString(h, s)
+	}
+	sum := h.Sum(nil)
+	sum[6] = (sum[6] & 0x0f) | 0x30
+	sum[8] = (sum[8] & 0x3f) | 0x80
+	id, err := uuid.FromBytes(sum)
+	if err != nil {
+		panic(err)
+	}
+	return id.String()
 }
