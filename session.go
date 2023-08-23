894fd0d5c2728baba5d8a310c3fd7758a2de34f8
Crossle Song <crosslesong@gmail.com>
2023-08-23 13:15:25 +0800

    add fetch session api

---
+++ session.go
@@ -0,0 +1,41 @@
+package bot
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type UserSession struct {
+	UserId    string `json:"user_id"`
+	SessionId string `json:"session_id"`
+	PublicKey string `json:"public_key"`
+	Platform  string `json:"platform"`
+}
+
+func FetchUserSession(ctx context.Context, users []string, uid, sid, sessionKey string) ([]*UserSession, error) {
+	data, err := json.Marshal(users)
+	if err != nil {
+		return nil, err
+	}
+	method, path := "POST", "/sessions/fetch"
+	token, err := SignAuthenticationToken(uid, sid, sessionKey, method, path, string(data))
+	if err != nil {
+		return nil, err
+	}
+	body, err := Request(ctx, "POST", path, data, token)
+	if err != nil {
+		return nil, ServerError(ctx, err)
+	}
+	var resp struct {
+		Data  []*UserSession `json:"data"`
+		Error Error          `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, BadDataError(ctx)
+	}
+	if resp.Error.Code > 0 {
+		return nil, resp.Error
+	}
+	return resp.Data, nil
+}
