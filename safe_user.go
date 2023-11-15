013674004744b0b91900285e87f49fbe67674055
Crossle Song <crosslesong@gmail.com>
2023-11-15 13:15:44 +0400

    update register safe

--- safe_user.go
+++ safe_user.go
@@ -11,35 +11,40 @@ import (
 	"github.com/MixinNetwork/mixin/crypto"
 )
 
-func RegisterSafe(ctx context.Context, userId, public, seed, saltBase64 string, uid, sid, sessionKey, pin, pinToken string) (*User, error) {
+func RegisterSafe(ctx context.Context, userId, public, seed string, uid, sid, sessionKey, tipPin, pinToken string) (*User, error) {
 	s, err := hex.DecodeString(seed)
-	if err != nil {
-		panic(err)
-	}
-	encryptedPIN, err := EncryptPIN(pin, pinToken, sid, sessionKey, uint64(time.Now().UnixNano()))
 	if err != nil {
 		return nil, err
 	}
 	private := ed25519.NewKeyFromSeed(s)
 	h := crypto.Sha256Hash([]byte(userId))
 	signBytes := ed25519.Sign(private, h[:])
-	sign := base64.RawURLEncoding.EncodeToString(signBytes[:])
+	signature := base64.RawURLEncoding.EncodeToString(signBytes[:])
 
+	tipBody := TIPBodyForSequencerRegister(userId, public)
+	pinBuf, err := hex.DecodeString(tipPin)
+	if err != nil {
+		return nil, err
+	}
+	sigBuf := ed25519.Sign(ed25519.PrivateKey(pinBuf), tipBody)
+
+	encryptedPIN, err := EncryptEd25519PIN(hex.EncodeToString(sigBuf), pinToken, sessionKey, uint64(time.Now().UnixNano()))
+	if err != nil {
+		return nil, err
+	}
 	data, _ := json.Marshal(map[string]string{
-		"public_key":  public,
-		"signature":   sign,
-		"user_id":     userId,
-		"pin_base64":  encryptedPIN,
-		"salt_base64": saltBase64,
+		"public_key": public,
+		"signature":  signature,
+		"pin_base64": encryptedPIN,
 	})
 
-	token, err := SignAuthenticationToken(uid, sid, privateKey, "POST", "/safe/users", string(data))
+	token, err := SignAuthenticationToken(uid, sid, sessionKey, "POST", "/safe/users", string(data))
 	if err != nil {
 		return nil, err
 	}
 	body, err := Request(ctx, "POST", "/safe/users", data, token)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	var resp struct {
 		Data  *User  `json:"data"`
