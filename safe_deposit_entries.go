3370ef8a14c688b51a6908a4238953cbc7155ffd
HunterQ <im.yuqlee@gmail.com>
2023-11-30 00:22:50 +0800

    create deposit entries (#133)

---
+++ safe_deposit_entries.go
@@ -0,0 +1,48 @@
+package bot
+
+import (
+	"context"
+	"encoding/json"
+)
+
+type DepositEntryView struct {
+	Type          string   `json:"type"`
+	EntryID       string   `json:"entry_id"`
+	Threshold     int64    `json:"threshold"`
+	Members       []string `json:"members"`
+	Destination   string   `json:"destination"`
+	Tag           string   `json:"tag"`
+	SafeSignature string   `json:"signature"`
+	ChainID       string   `json:"chain_id"`
+	IsPrimary     bool     `json:"is_primary"`
+}
+
+func CreateDepositEntry(ctx context.Context, chainID string, members []string, threshold int64) ([]*DepositEntryView, error) {
+	data, _ := json.Marshal(map[string]any{
+		"chain_id":  chainID,
+		"members":   members,
+		"threshold": threshold,
+	})
+	endpoint := "/safe/deposit/entries"
+
+	token, err := SignAuthenticationToken(uid, sid, privateKey, "POST", endpoint, string(data))
+	if err != nil {
+		return nil, err
+	}
+	body, err := Request(ctx, "POST", "/users", data, token)
+	if err != nil {
+		return nil, ServerError(ctx, err)
+	}
+	var resp struct {
+		Data  []*DepositEntryView `json:"data"`
+		Error Error               `json:"error"`
+	}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, BadDataError(ctx)
+	}
+	if resp.Error.Code > 0 {
+		return nil, resp.Error
+	}
+	return resp.Data, nil
+}
