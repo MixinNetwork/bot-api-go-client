19b2d75a56fd54cb4250a9d1a52debce1a18cf53
Cedric Fung <cedric@vec.io>
2023-12-09 17:04:01 +0000

    send object storage transaction

---
+++ object.go
@@ -0,0 +1,24 @@
+package bot
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/MixinNetwork/mixin/common"
+)
+
+func CreateObjectStorageTransaction(ctx context.Context, extra []byte, traceId string, u *SafeUser) (*SequencerTransactionRequest, error) {
+	if len(extra) > common.ExtraSizeStorageCapacity {
+		return nil, fmt.Errorf("too large extra %d > %d", len(extra), common.ExtraSizeStorageCapacity)
+	}
+	step := common.NewIntegerFromString(common.ExtraStoragePriceStep)
+	amount := step.Mul(len(extra)/common.ExtraSizeStorageStep + 1)
+	addr := common.NewAddressFromSeed(make([]byte, 64))
+	mix := NewMainnetMixAddress([]string{addr.String()}, 1)
+	mix.Threshold = 64
+	recipients := []*TransactionRecipient{{
+		MixAddress: mix.String(),
+		Amount:     amount.String(),
+	}}
+	return SendTransaction(ctx, common.XINAssetId.String(), recipients, traceId, extra, nil, u)
+}
