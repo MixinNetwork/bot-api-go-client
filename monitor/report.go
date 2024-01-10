d9ce44b0acab01f8d6fe075d5a90f9d51ecd7af7
HunterQ <im.yuqlee@gmail.com>
2024-01-10 15:24:24 +1100

    Feature/monitor (#147)
    * Add monitor
    
    * Add monitor
    
    * Fix name

---
+++ monitor/report.go
@@ -0,0 +1,76 @@
+package monitor
+
+import (
+	"context"
+	"encoding/hex"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/MixinNetwork/bot-api-go-client/v3"
+	yaml "gopkg.in/yaml.v3"
+)
+
+const (
+	AppMessageRunningPeriod = "period"
+	AppMessageRunningTime   = "time"
+)
+
+type MessageData struct {
+	Name  string `yaml:"name" json:"name"`
+	Value string `yaml:"value" json:"value"`
+	Score int    `yaml:"score" json:"score"`
+}
+
+type AppMessage struct {
+	Project string         `yaml:"project"`
+	Running string         `yaml:"running"`
+	Status  int            `yaml:"status"`
+	Data    []*MessageData `yaml:"data"`
+}
+
+func UnmarshalAppMessage(b []byte) (*AppMessage, error) {
+	var m *AppMessage
+	err := yaml.Unmarshal(b, &m)
+	return m, err
+}
+
+func (m *AppMessage) Marshal() ([]byte, error) {
+	b, err := yaml.Marshal(m)
+	return b, err
+}
+
+func ReportToMonitor(ctx context.Context, asset, amount, trace string, receivers []string, threshold int, msg *AppMessage, u *bot.SafeUser) (*bot.SequencerTransactionRequest, error) {
+	minutes := time.Now().UTC().Unix() / 60
+	memo, err := msg.Marshal()
+	if err != nil {
+		return nil, err
+	}
+	ma := bot.NewUUIDMixAddress(receivers, byte(threshold))
+	tr := &bot.TransactionRecipient{MixAddress: ma.String(), Amount: amount}
+	if trace == "" {
+		trace = bot.UniqueObjectId(ma.String(), asset, amount, u.UserId, hex.EncodeToString(memo), fmt.Sprint(minutes))
+	}
+	old, err := bot.GetTransactionById(ctx, trace)
+	if err != nil || old != nil {
+		return nil, err
+	}
+	return bot.SendTransaction(ctx, asset, []*bot.TransactionRecipient{tr}, trace, memo, nil, u)
+}
+
+func CheckRetryableError(err error) bool {
+	if err == nil {
+		return false
+	}
+	reason := strings.ToLower(err.Error())
+	switch {
+	case strings.Contains(reason, "timeout"):
+	case strings.Contains(reason, "internal server"):
+	case strings.Contains(reason, "insufficient"):
+	case strings.Contains(reason, "inputs locked by"): // concurrent utxo query
+	case strings.Contains(reason, "by other transaction"): // concurrent utxo query
+	default:
+		return false
+	}
+	return true
+}
