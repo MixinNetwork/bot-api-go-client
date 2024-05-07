2b12eb2ee7004d65e3bd46e947b3cd44c5aa9323
HunterQ <im.yuqlee@gmail.com>
2024-05-07 19:21:14 +1000

    add object limit amount (#173)

--- object.go
+++ object.go
@@ -7,12 +7,18 @@ import (
 	"github.com/MixinNetwork/mixin/common"
 )
 
-func CreateObjectStorageTransaction(ctx context.Context, extra []byte, traceId string, references []string, u *SafeUser) (*SequencerTransactionRequest, error) {
+func CreateObjectStorageTransaction(ctx context.Context, extra []byte, traceId string, references []string, limit string, u *SafeUser) (*SequencerTransactionRequest, error) {
 	if len(extra) > common.ExtraSizeStorageCapacity {
 		return nil, fmt.Errorf("too large extra %d > %d", len(extra), common.ExtraSizeStorageCapacity)
 	}
 	step := common.NewIntegerFromString(common.ExtraStoragePriceStep)
 	amount := step.Mul(len(extra)/common.ExtraSizeStorageStep + 1)
+	if limit != "" {
+		strl := common.NewIntegerFromString(limit)
+		if strl.Cmp(amount) > 0 {
+			amount = strl
+		}
+	}
 	addr := common.NewAddressFromSeed(make([]byte, 64))
 	mix := NewMainnetMixAddress([]string{addr.String()}, 1)
 	mix.Threshold = 64
