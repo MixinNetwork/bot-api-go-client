8d2d4c3722f0bc772d86f0e6ca20015f1a5df2c8
touge <touge1027@gmail.com>
2024-12-10 19:33:45 +0800

    tweak withdraw

--- withdrawal.go
+++ withdrawal.go
@@ -50,11 +50,15 @@ func SendWithdrawal(ctx context.Context, assetId, destination, tag, amount, trac
 		fee = chainFee
 	}
 
-	return withdrawalTransaction(ctx, traceId, MixinFeeUserId, fee.AssetID, fee.Amount, assetId, destination, tag, memo, amount, u)
+	return withdrawalTransaction(ctx, traceId, MixinFeeUserId, fee.AssetID, fee.Amount, assetId, destination, tag, memo, amount, nil, u)
+}
+
+func WithdrawalWithFeeUtxos(ctx context.Context, traceId, feeAssetId, feeAmount, assetId, destination, tag, memo, amount string, feeUtxos []*Output, u *SafeUser) ([]*SequencerTransactionRequest, error) {
+	return withdrawalTransaction(ctx, traceId, MixinFeeUserId, feeAssetId, feeAmount, assetId, destination, tag, memo, amount, feeUtxos, u)
 }
 
 func withdrawalTransaction(ctx context.Context, traceId, feeReceiverId string, feeAssetId string, feeAmount,
-	assetId, destination, tag, memo, amount string, u *SafeUser) ([]*SequencerTransactionRequest, error) {
+	assetId, destination, tag, memo, amount string, feeUtxos []*Output, u *SafeUser) ([]*SequencerTransactionRequest, error) {
 	isFeeDifferentAsset := feeAssetId != assetId
 	asset := crypto.Sha256Hash([]byte(assetId))
 	if isFeeDifferentAsset {
@@ -83,9 +87,21 @@ func withdrawalTransaction(ctx context.Context, traceId, feeReceiverId string, f
 				MixAddress: NewUUIDMixAddress([]string{feeReceiverId}, 1),
 			},
 		}
-		feeUtxos, feeChange, err := requestUnspentOutputsForRecipients(ctx, feeAssetId, feeRecipients, u)
-		if err != nil {
-			return nil, err
+
+		var feeChange common.Integer
+		if len(feeUtxos) > 0 {
+			totalFeeOutput := common.NewIntegerFromString(feeAmount)
+			var totalFeeInput common.Integer
+			for _, o := range feeUtxos {
+				amt := common.NewIntegerFromString(o.Amount)
+				totalFeeInput = totalFeeInput.Add(amt)
+			}
+			feeChange = totalFeeInput.Sub(totalFeeOutput)
+		} else {
+			feeUtxos, feeChange, err = requestUnspentOutputsForRecipients(ctx, feeAssetId, feeRecipients, u)
+			if err != nil {
+				return nil, err
+			}
 		}
 		if feeChange.Sign() > 0 {
 			feeRecipients = append(feeRecipients, &TransactionRecipient{
@@ -181,117 +197,4 @@ func withdrawalTransaction(ctx context.Context, traceId, feeReceiverId string, f
 		}
 		return []*SequencerTransactionRequest{tx}, nil
 	}
-}
-
-func WithdrawalTransactionWithFeeUtxos(ctx context.Context, traceId, feeAssetId, feeAmount, assetId, destination, tag, memo, amount string, feeUtxos []*Output, u *SafeUser) ([]*SequencerTransactionRequest, error) {
-	asset := crypto.Sha256Hash([]byte(assetId))
-	feeTraceId := UniqueObjectId(traceId, "FEE")
-	feeAsset := crypto.Sha256Hash([]byte(feeAssetId))
-	recipients := []*TransactionRecipient{
-		{
-			Amount:      amount,
-			Destination: destination,
-			Tag:         tag,
-		},
-	}
-	utxos, change, err := requestUnspentOutputsForRecipients(ctx, assetId, recipients, u)
-	if err != nil {
-		return nil, err
-	}
-	if change.Sign() > 0 {
-		recipients = append(recipients, &TransactionRecipient{
-			Amount:     change.String(),
-			MixAddress: NewUUIDMixAddress([]string{u.UserId}, 1),
-		})
-	}
-	feeRecipients := []*TransactionRecipient{
-		{
-			Amount:     feeAmount,
-			MixAddress: NewUUIDMixAddress([]string{MixinFeeUserId}, 1),
-		},
-	}
-
-	totalFeeOutput := common.NewIntegerFromString(feeAmount)
-	var totalFeeInput common.Integer
-	for _, o := range feeUtxos {
-		amt := common.NewIntegerFromString(o.Amount)
-		totalFeeInput = totalFeeInput.Add(amt)
-	}
-	feeChange := totalFeeInput.Sub(totalFeeOutput)
-	if feeChange.Sign() > 0 {
-		feeRecipients = append(feeRecipients, &TransactionRecipient{
-			Amount:     feeChange.String(),
-			MixAddress: NewUUIDMixAddress([]string{u.UserId}, 1),
-		})
-	}
-
-	transaction, err := BuildRawTransaction(ctx, asset, utxos, recipients, []byte(memo), nil, traceId, u)
-	if err != nil {
-		return nil, fmt.Errorf("BuildRawTransaction(%s): %w", asset, err)
-	}
-	ver := transaction.AsVersioned()
-	feeTransaction, err := BuildRawTransaction(ctx, feeAsset, feeUtxos, feeRecipients, []byte(memo), []string{crypto.Blake3Hash(ver.Marshal()).String()}, feeTraceId, u)
-	if err != nil {
-		return nil, fmt.Errorf("buildFeeRawTransaction(%s): %w", feeAsset, err)
-	}
-	feeVer := feeTransaction.AsVersioned()
-
-	requests, err := VerifyRawTransaction(ctx, []*KernelTransactionRequestCreateRequest{
-		{
-			RequestID: traceId,
-			Raw:       hex.EncodeToString(ver.Marshal()),
-		},
-		{
-			RequestID: feeTraceId,
-			Raw:       hex.EncodeToString(feeVer.Marshal()),
-		},
-	}, u)
-	if err != nil {
-		return nil, err
-	} else if len(requests) != 2 {
-		return nil, fmt.Errorf("invalid requests count %d", len(requests))
-	} else if requests[0].State != "unspent" {
-		return nil, fmt.Errorf("invalid transaction state %s", requests[0].State)
-	}
-	var str *SequencerTransactionRequest
-	var feeStr *SequencerTransactionRequest
-	for _, r := range requests {
-		if r.RequestID == traceId {
-			str = r
-		} else if r.RequestID == feeTraceId {
-			feeStr = r
-		}
-	}
-	if str == nil || feeStr == nil {
-		return nil, fmt.Errorf("invalid sequencer transaction requests")
-	}
-	if len(str.Views) != len(ver.Inputs) {
-		return nil, fmt.Errorf("invalid inputs count %d/%d", len(str.Views), len(ver.Inputs))
-	}
-	if len(feeStr.Views) != len(feeVer.Inputs) {
-		return nil, fmt.Errorf("invalid fee inputs count %d/%d", len(feeStr.Views), len(feeVer.Inputs))
-	}
-
-	ver, err = signRawTransaction(ver, str.Views, u.SpendPrivateKey)
-	if err != nil {
-		return nil, fmt.Errorf("signRawTransaction(%s): %w", asset, err)
-	}
-	feeVer, err = signRawTransaction(feeVer, feeStr.Views, u.SpendPrivateKey)
-	if err != nil {
-		return nil, fmt.Errorf("signFeeRawTransaction(%s): %w", feeAsset, err)
-	}
-	results, err := SendRawTransaction(ctx, []*KernelTransactionRequestCreateRequest{
-		{
-			RequestID: traceId,
-			Raw:       hex.EncodeToString(ver.Marshal()),
-		},
-		{
-			RequestID: feeTraceId,
-			Raw:       hex.EncodeToString(feeVer.Marshal()),
-		},
-	}, u)
-	if err != nil {
-		return nil, fmt.Errorf("SendRawTransaction(%s): %w", traceId, err)
-	}
-	return results, nil
 }
