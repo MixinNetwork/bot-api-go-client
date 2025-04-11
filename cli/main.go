f11aa2b341aef7aacedca212a8451e018a060b9f
li <yuqlee@mixin.one>
2025-04-11 08:22:31 +0800

    Add safe_ghost_keys command

--- cli/main.go
+++ cli/main.go
@@ -29,6 +29,7 @@ func main() {
 			safeOutputsCmdCli,
 			safeOutputCmdCli,
 			safeMultisigRequestCmdCli,
+			safeGhostKeysCmdCli,
 			withdrawalCmdCli,
 			requestDepositEntryCmdCli,
 			buildMixAddressCmdCli,
