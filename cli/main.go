10aea69f59aecc90b83f4667f02d4162ec4d1cec
li <yuqlee@mixin.one>
2025-07-03 12:06:48 +0800

    load bare user

--- cli/main.go
+++ cli/main.go
@@ -60,3 +60,29 @@ func loadKeystore(keystore string) *bot.SafeUser {
 	}
 	return &u
 }
+
+type BareUser struct {
+	UserId            string `json:"user_id"`
+	SessionId         string `json:"session_id"`
+	SessionPrivateKey string `json:"session_private_key"`
+	ServerPublicKey   string `json:"server_public_key"`
+}
+
+func loadKeystoreBareUser(keystore string) *bot.SafeUser {
+	dat, err := os.ReadFile(keystore)
+	if err != nil {
+		panic(err)
+	}
+	var bu BareUser
+	err = json.Unmarshal([]byte(dat), &bu)
+	if err != nil {
+		panic(err)
+	}
+	u := bot.SafeUser{
+		UserId:            bu.UserId,
+		SessionId:         bu.SessionId,
+		SessionPrivateKey: bu.SessionPrivateKey,
+		ServerPublicKey:   bu.ServerPublicKey,
+	}
+	return &u
+}
