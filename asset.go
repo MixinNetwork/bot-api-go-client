7dd6bec8317d42ace2fbe4355e8656e251516d58
Crossle Song <crosslesong@gmail.com>
2026-09-02 11:27:28 +0400

    add usdg asset

--- asset.go
+++ asset.go
@@ -37,6 +37,8 @@ const (
 	USDC_HYPEREVM = "1e01fede-51fa-3791-9b06-5c18801b272c"
 	USDC_XLAYER   = "8d706a25-514c-3c73-9446-c25fd07d0ae2"
 	USDC_SUI      = "a0f7ad61-3b9f-30f3-a1de-cd831aec33ff"
+
+	USDG_ROBINHOOD = "e2d1872a-abd5-3036-aacb-c90570cc7fb4"
 )
 
 type Asset struct {
